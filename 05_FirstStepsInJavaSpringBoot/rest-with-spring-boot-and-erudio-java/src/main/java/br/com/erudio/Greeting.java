571f2eb7971514c0fda02d51e53ceacd6186fc82
RichardSantosPaiva <richardpaiva0101@gmail.com>
2024-12-14 13:09:50 -0300

    criando calculadora da aula 06_ParametersAndExceptionHandler

--- 05_FirstStepsInJavaSpringBoot/rest-with-spring-boot-and-erudio-java/src/main/java/br/com/erudio/Greeting.java
+++ 05_FirstStepsInJavaSpringBoot/rest-with-spring-boot-and-erudio-java/src/main/java/br/com/erudio/Greeting.java
@@ -1,14 +1,10 @@
 package br.com.erudio;
 
 public class Greeting {
+	
 	private final long id;
 	private final String content;
-
-	public Greeting(long id, String content) {
-		this.id = id;
-		this.content = content;
-	}
-
+	
 	public long getId() {
 		return id;
 	}
@@ -16,6 +12,10 @@ public class Greeting {
 	public String getContent() {
 		return content;
 	}
-	
+
+	public Greeting(long id, String content) {
+		this.id = id;
+		this.content = content;
+	}
 	
 }
